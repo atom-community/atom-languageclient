08a507de6a90f949a614af9579c3a0f6a75e942b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-18 03:48:26 -0600

    chore: reexport atom-ide-base types as atom-ide

--- typings/atom-ide/index.d.ts
+++ typings/atom-ide/index.d.ts
@@ -1,2 +1,9 @@
 declare module 'atom-ide' {
+    // atom-ide-base types for backward compatibility
+    export * from "atom-ide-base/types-packages/main"
+
+    // NotificationButton for backward compatibility (moved to "../../lib/adapters/notifications-adapter")
+    export interface NotificationButton {
+      text: string
+    }
 }
