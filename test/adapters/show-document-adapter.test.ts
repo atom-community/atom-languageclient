ef3bf1a0c3857e8946b65c94e38de5609a3c8bbc
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-16 20:51:56 -0500

    test: add test for ShowDocumentAdapter attaching

---
+++ test/adapters/show-document-adapter.test.ts
@@ -0,0 +1,22 @@
+import * as sinon from "sinon"
+import { expect } from "chai"
+import * as ShowDocumentAdapter from "../../lib/adapters/show-document-adapter"
+import { LanguageClientConnection } from "../../lib/languageclient"
+import { createSpyConnection } from "../helpers"
+
+describe("ShowDocumentAdapter", () => {
+  describe("can attach to a server", () => {
+    const lcc = new LanguageClientConnection(createSpyConnection())
+
+    it("subscribes to onShowDocument", async () => {
+      const spy = sinon.spy()
+      lcc["_onRequest"] = spy
+
+      ShowDocumentAdapter.attach(lcc)
+      expect((lcc["_onRequest"] as sinon.SinonSpy).calledOnce).to.be.true
+      const spyArgs = spy.firstCall.args
+      expect(spyArgs[0]).to.deep.equal({ method: "window/showDocument" })
+      expect(spyArgs[1]).to.equal(ShowDocumentAdapter.onShowDocument)
+    })
+  })
+})
