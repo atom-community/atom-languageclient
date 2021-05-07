3b49eb3ab1c5c6dd6ef86d429c81588fa596a709
Tony Brix <tony@brix.ninja>
2021-05-06 23:47:21 -0500

    chore: remove build before tests

--- test/runner.js
+++ test/runner.js
@@ -1,8 +1,4 @@
 const { createRunner } = require("atom-jasmine3-test-runner")
-const { execSync } = require("child_process")
-const path = require("path")
-
-execSync("npm run build", { cwd: path.resolve(__dirname, "..") })
 
 module.exports = createRunner({
   suffix: ".test",
