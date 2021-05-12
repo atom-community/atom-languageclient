43d6eaa832c56dc1ad746f454af2c4055c15518e
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-05-12 02:26:23 -0500

    test: make createSpyConnection more realistic by adding return values

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,28 +1,37 @@
 import * as rpc from "vscode-jsonrpc"
 import { TextEditor } from "atom"
 import AutoLanguageClient from "../lib/auto-languageclient"
-import { LanguageClientConnection } from "../lib/languageclient"
+import { LanguageClientConnection, Disposable, Event } from "../lib/languageclient"
 import { LanguageServerProcess } from "../lib/server-manager"
 import { spawn } from "spawk"
 
+function createSpyDisposable(): Disposable {
+  return { dispose: jasmine.createSpy("dispose") }
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+function createSpyEvent(): Event<Disposable> {
+  return jasmine.createSpy("event").and.returnValue(createSpyDisposable())
+}
+
 export function createSpyConnection(): rpc.MessageConnection {
   return {
-    listen: jasmine.createSpy("listen"),
-    onClose: jasmine.createSpy("onClose"),
-    onError: jasmine.createSpy("onError"),
-    onDispose: jasmine.createSpy("onDispose"),
-    onUnhandledNotification: jasmine.createSpy("onUnhandledNotification"),
-    onUnhandledProgress: jasmine.createSpy("onUnhandledProgress"),
-    onRequest: jasmine.createSpy("onRequest"),
-    onNotification: jasmine.createSpy("onNotification"),
-    onProgress: jasmine.createSpy("onProgress"),
-    dispose: jasmine.createSpy("dispose"),
-    sendRequest: jasmine.createSpy("sendRequest"),
-    sendNotification: jasmine.createSpy("sendNotification"),
-    sendProgress: jasmine.createSpy("sendProgress"),
-    trace: jasmine.createSpy("trace"),
-    inspect: jasmine.createSpy("inspect"),
-    end: jasmine.createSpy("end"),
+    listen: jasmine.createSpy("listen").and.returnValue(void 0),
+    onClose: jasmine.createSpy("onClose").and.returnValue(createSpyEvent()),
+    onError: jasmine.createSpy("onError").and.returnValue(createSpyEvent()),
+    onDispose: jasmine.createSpy("onDispose").and.returnValue(createSpyEvent()),
+    onUnhandledNotification: jasmine.createSpy("onUnhandledNotification").and.returnValue(createSpyEvent()),
+    onUnhandledProgress: jasmine.createSpy("onUnhandledProgress").and.returnValue(createSpyEvent()),
+    onRequest: jasmine.createSpy("onRequest").and.returnValue(createSpyDisposable()),
+    onNotification: jasmine.createSpy("onNotification").and.returnValue(createSpyDisposable()),
+    onProgress: jasmine.createSpy("onProgress").and.returnValue(createSpyDisposable()),
+    dispose: jasmine.createSpy("dispose").and.returnValue(void 0),
+    sendRequest: jasmine.createSpy("sendRequest").and.resolveTo(true),
+    sendNotification: jasmine.createSpy("sendNotification").and.returnValue(void 0),
+    sendProgress: jasmine.createSpy("sendProgress").and.returnValue(void 0),
+    trace: jasmine.createSpy("trace").and.returnValue(void 0),
+    inspect: jasmine.createSpy("inspect").and.returnValue(void 0),
+    end: jasmine.createSpy("end").and.returnValue(void 0),
   }
 }
 
